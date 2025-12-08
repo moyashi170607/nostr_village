b0f20995370c7032529515d5770c9d5bad9806dc
こつ子 <96937973+moyashi170607@users.noreply.github.com>
2025-12-09 00:43:17 +0900

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    // GitHub Pagesへのデプロイに必須
+    // base: '/<リポジトリ名>/' を指定する
+    base: '/nostr_village/',
+
+    // その他の設定
+    plugins: [
+        // ...
+    ],
+    build: {
+        // ...
+    }
+});
