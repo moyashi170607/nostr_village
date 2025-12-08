4bf342e6b6e169a5635bd5a953a516fe43ba2861
こつ子 <96937973+moyashi170607@users.noreply.github.com>
2025-12-09 01:27:06 +0900

    rootの修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // GitHub Pagesへのデプロイに必須
-    // base: '/<リポジトリ名>/' を指定する
     base: './',
-
     // その他の設定
     plugins: [
-        // ...
+
     ],
     build: {
-        // ...
     }
 });
