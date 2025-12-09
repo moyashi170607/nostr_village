e1a283428ec2ea683800ba8521b385479a00c029
こつ子 <96937973+moyashi170607@users.noreply.github.com>
2025-12-10 00:09:12 +0900

    デフォルトリレー

---
+++ src/nostr/nostr.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_RELAY_LIST: string[] = [
+    'wss://nostr.wine/',
+    'wss://relay.snort.social/',
+    'wss://wot.utxo.one/',
+    'wss://nostrelites.org/',
+    'https://yabu.me',
+    'https://nostrcheck.me',
+    'https://nostr.build'
+]
+
+export let relay: string[] = []
