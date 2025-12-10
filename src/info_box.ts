a3a8b410631aaab91414c0b6b679b770d39bb8fb
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2025-12-11 02:25:17 +0900

    テキスト2つ追加

--- src/info_box.ts
+++ src/info_box.ts
@@ -1,6 +1,18 @@
 import Phaser from "phaser";
 import { GAME_DEFAULT_HEIGHT, GAME_DEFAULT_WIDTH } from "./main";
 
+const HOME_NAME_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = {
+    padding: { top: 5 },
+    fontSize: 40,
+    color: "#000000"
+}
+
+const OWNER_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = {
+    padding: { top: 5 },
+    fontSize: 35,
+    color: "#000000"
+}
+
 
 /**
  * 家の情報を表示するボックス
@@ -24,25 +36,29 @@ export class InfoBox extends Phaser.GameObjects.Container {
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y)
 
+
         this.setScrollFactor(0)
+        this.setDepth(5);
 
 
         //色を設定
         const BACK_FILL_COLOR: number = 0xe3e3e3;
         const BACK_STROKE_COLOR: number = 0x404040;
 
-        this.backRect = this.scene.add.rectangle(30, 5, GAME_DEFAULT_WIDTH / 2.5, GAME_DEFAULT_HEIGHT - 30, BACK_FILL_COLOR)
+        this.backRect = this.scene.add.rectangle(0, 0, GAME_DEFAULT_WIDTH / 2.5, GAME_DEFAULT_HEIGHT - 200, BACK_FILL_COLOR)
         this.backRect.setStrokeStyle(5, BACK_STROKE_COLOR);
-        this.backRect.setDepth(5)
         this.backRect.setOrigin(0, 0)
 
+        this.backRect.setInteractive()
 
-
-        //this.scene.add.existing(this.backRect)
+        this.homeNameText = this.scene.add.text(10, 10, "こつ子ハウス", HOME_NAME_TEXT_STYLE)
+        this.ownerText = this.scene.add.text(10, 60, "こつ子", OWNER_TEXT_STYLE)
 
 
         this.add([
-            this.backRect
+            this.backRect,
+            this.homeNameText,
+            this.ownerText
         ])
 
         this.scene.add.existing(this)
