70123b80bed75cf1b365d4a2a7e9a108d963ee24
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2025-12-12 01:59:28 +0900

    info_box

--- src/info_box.ts
+++ src/info_box.ts
@@ -13,6 +13,24 @@ const OWNER_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = {
     color: "#000000"
 }
 
+const PUBKEY_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = {
+    padding: { top: 5 },
+    fontSize: 30,
+    color: "#000000"
+}
+
+const GREETING_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = {
+    padding: { top: 5 },
+    fontSize: 30,
+    color: "#000000"
+}
+
+const POSITION_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = {
+    padding: { top: 5 },
+    fontSize: 30,
+    color: "#000000"
+}
+
 
 /**
  * 家の情報を表示するボックス
@@ -28,10 +46,14 @@ export class InfoBox extends Phaser.GameObjects.Container {
     homeNameText!: Phaser.GameObjects.Text
     //持ち主の名前
     ownerText!: Phaser.GameObjects.Text
+    //公開鍵
+    pubkeyText!: Phaser.GameObjects.Text
     //一言コメント
     greetingText!: Phaser.GameObjects.Text
     //座標
     positionText!: Phaser.GameObjects.Text
+    //アイコン
+    iconImage!: Phaser.GameObjects.Image
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y)
@@ -53,12 +75,19 @@ export class InfoBox extends Phaser.GameObjects.Container {
 
         this.homeNameText = this.scene.add.text(10, 10, "こつ子ハウス", HOME_NAME_TEXT_STYLE)
         this.ownerText = this.scene.add.text(10, 60, "こつ子", OWNER_TEXT_STYLE)
+        this.pubkeyText = this.scene.add.text(10, 110, "公開鍵", PUBKEY_TEXT_STYLE)
+
+        this.greetingText = this.scene.add.text(10, 160, "一言コメント", GREETING_TEXT_STYLE)
+        this.positionText = this.scene.add.text(10, 260, "住所（x,y）", POSITION_TEXT_STYLE)
 
 
         this.add([
             this.backRect,
             this.homeNameText,
-            this.ownerText
+            this.ownerText,
+            this.pubkeyText,
+            this.greetingText,
+            this.positionText
         ])
 
         this.scene.add.existing(this)
